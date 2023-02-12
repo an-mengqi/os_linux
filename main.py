f3fdb0a2f761cdc79e4893507fd89b767e2b99c0
Anastasiia Monakhova <a.klopova@dssl.ru>
2023-02-12 18:19:38 +0100

    main.py improved, old result file deleted, file with new result added

--- main.py
+++ main.py
@@ -4,60 +4,19 @@ from subprocess import (
 )
 
 
-result = run(["ps", "aux"], stderr=PIPE, stdout=PIPE)
-
-stdout_lines = result.stdout.splitlines()
-
-processes_splitted_lines = []
-for line in stdout_lines[1:]:
-    splitted_line = line.split()
-    processes_splitted_lines.append(splitted_line)
-
-
 class Process:
 
-    def __init__(self, user, pid, cpu, memory, virt_mem, rss, tt, state, starting_time, cpu_time, command):
+    def __init__(self, user, cpu, memory, rss, command):
         self.user = user.decode('utf-8')
-        self.pid = pid
         self.cpu = cpu.decode('utf-8')
         self.memory = memory.decode('utf-8')
-        self.virt_mem = virt_mem
-        self.rss = rss
-        self.tt = tt
-        self.state = state
-        self.starting_time = starting_time
-        self.cpu_time = cpu_time
+        self.rss = rss.decode('utf-8')
         self.command = command.decode('utf-8')
 
 
-processes_list = []
-for line in processes_splitted_lines:
-    process = Process(line[0],
-                      line[1],
-                      line[2],
-                      line[3],
-                      line[4],
-                      line[5],
-                      line[6],
-                      line[7],
-                      line[8],
-                      line[9],
-                      line[10]
-                      )
-    processes_list.append(process)
-
-
-def get_all_users():
-    all_users = []
-    for element in processes_list:
-        user = element.user
-        all_users.append(user)
-    return all_users
-
-
 def get_unique_users():
-    all_users_res = get_all_users()
-    unique_users = set(all_users_res)
+    all_users = [element.user for element in processes_list]
+    unique_users = set(all_users)
     return unique_users
 
 
@@ -79,19 +38,18 @@ def count_each_user_processes():
         if decoded_element not in result:
             result[decoded_element] = 1
         else:
-            result[decoded_element] = result[decoded_element] + 1
-    res_string = ""
-    for key, value in result.items():
-        res_string += key + ':' + str(value) + '\n'
+            result[decoded_element] += 1
+    res_string = '\n'.join([f'{key}: {value}' for key, value in result.items()])
     return res_string
 
 
 def count_all_memory_used():
     all_memory = 0
     for element in processes_list:
-        decoded_element = element.memory
+        decoded_element = element.rss
         all_memory = all_memory + float(decoded_element)
-    return str(round(all_memory, 1))
+    mem_mb = all_memory/1000
+    return str(round(mem_mb, 1))
 
 
 def count_all_cpu_percent():
@@ -118,19 +76,40 @@ def find_process_with_max_mem_usage():
     return max_mem_element.command[:20]
 
 
-text = f"""Отчёт о состоянии системы:
-Пользователи системы: {get_system_unique_users()}
-Процессов запущено: {count_all_processes()}
-Пользовательских процессов:
-{count_each_user_processes()}
-Всего памяти используется: {count_all_memory_used()} mb
-Всего CPU используется: {count_all_cpu_percent()} %
-Больше всего памяти использует: {find_process_with_max_mem_usage()}
-Больше всего CPU использует: {find_process_with_max_cpu_usage()}
-"""
+if __name__ == '__main__':
 
-now = datetime.now()
-dt_string = now.strftime("%d-%m-%Y-%H:%M")
+    result = run(["ps", "aux"], stderr=PIPE, stdout=PIPE)
 
-with open(f'{dt_string}-scan.txt', 'w') as f:
-    f.write(text)
+    stdout_lines = result.stdout.splitlines()
+
+    processes_splitted_lines = []
+    for line in stdout_lines[1:]:
+        splitted_line = line.split()
+        processes_splitted_lines.append(splitted_line)
+
+    processes_list = []
+    for line in processes_splitted_lines:
+        process = Process(user=line[0],
+                          cpu=line[2],
+                          memory=line[3],
+                          rss=line[5],
+                          command=line[10]
+                          )
+        processes_list.append(process)
+
+    text = f"""Отчёт о состоянии системы:
+    Пользователи системы: {get_system_unique_users()}
+    Процессов запущено: {count_all_processes()}
+    Пользовательских процессов:
+    {count_each_user_processes()}
+    Всего памяти используется: {count_all_memory_used()} mb
+    Всего CPU используется: {count_all_cpu_percent()} %
+    Больше всего памяти использует: {find_process_with_max_mem_usage()}
+    Больше всего CPU использует: {find_process_with_max_cpu_usage()}
+    """
+
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y-%H:%M")
+
+    with open(f'{dt_string}-scan.txt', 'w') as f:
+        f.write(text)
